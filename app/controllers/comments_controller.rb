a3f51335d1b1bde23a365703f7f5b2be7ffa86f4
garedean <garrettdolson@gmail.com>
2015-07-12 16:53:33 -0700

    Add AJAX functionality for comment creation and deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,28 +5,28 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comments = @blog.comments.all
 
-    #if verify_recaptcha
-      if @comment.save
-        @blog.comments.push(@comment)
-        flash[:notice] = "Comment added!"
+    if @comment.save
+      @blog.comments.push(@comment)
+      flash[:notice] = "Comment added!"
 
-        respond_to do |format|
-          #format.html { redirect_to :back }
-          format.js { render "blogs/create" }
-        end
-      else
-        render "blogs/show"
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js { render "blogs/create" }
       end
-    # else
-    #   render "blogs/show"
-    # end
+    else
+      render "blogs/show"
+    end
   end
 
   def destroy
     @blog    = Blog.find(params[:blog_id])
     @comment = @blog.comments.find(params[:id])
     @comment.destroy
-    redirect_to :back
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render "blogs/destroy" }
+    end
   end
 
   private
