cc7368f2ab8a013a04d2f60e2f9a7d7899c09dfa
garedean <garrettdolson@gmail.com>
2015-08-17 10:43:09 -0700

    DRY up resume view with use of helpers

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,21 @@
 module StaticPagesHelper
+  def resume_block(&block)
+    content_tag :div, &block
+  end
+
+  def position_details(company_name, job_title, details)
+    company_name = content_tag(:strong, "#{company_name }, ")
+    job_title    = content_tag(:strong, "#{job_title} ")
+    details      = content_tag(:em, details)
+    content = company_name + job_title + details
+    content_tag(:p, content)
+  end
+
+  def position_highlights(&block)
+    content_tag :ul, &block
+  end
+
+  def bullet_point(&block)
+    content_tag(:li, &block)
+  end
 end
