126cc02b75c8c79f2726865941ae2042e5919a90
garedean <garrettdolson@gmail.com>
2015-09-08 18:16:20 -0700

    Make small styling improvements throughout the app

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -4,11 +4,6 @@ $(function() {
     $(this).parent().addClass("expanded");
   });
 
-  $(".clip-circle").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-    //window.location.replace("/about");
-  });
-
-  //$(".flash").delay(1500).slideUp(500);
   $(".flash").delay(1500).animate({
     top: "-26",
   }, 900, "easeOutBounce" );
