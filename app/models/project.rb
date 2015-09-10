f8818d90fdadd33784e5ac3b154468bc1f660719
garedean <garrettdolson@gmail.com>
2015-09-10 15:22:16 -0700

    Add placeholder for missing project screenshots

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,11 @@
 class Project < ActiveRecord::Base
   def screenshot_name
     target_file = name.downcase.gsub(" ", "_")
+
+    if Rails.application.assets.find_asset("screenshots/#{target_file}.jpg")
+      target_file
+    else
+      "screenshot_missing"
+    end
   end
 end
