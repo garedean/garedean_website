802a8f33a32422c8a472288e7b37b4338d49c484
garedean <garrettdolson@gmail.com>
2015-12-31 19:55:26 -0800

    Fix kindjournal screenshot not showing on 'work' page

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   def screenshot_name
     target_file = name.downcase.gsub(" ", "_")
 
-    if Rails.application.assets.find_asset("screenshots/#{target_file}.jpg")
+    if Rails.application.assets.find_asset("screenshots/#{target_file}")
       target_file
     else
       "screenshot_missing"
